8e23515540c1845252579f8f8a163b0dacdcf62f
Camila Alcantara <camilamariana2008@hotmail.com>
2020-08-20 23:57:13 -0300

    configurando cargo

--- apiZetta/src/main/java/com/zettaApp/apiZetta/resources/CargoResource.java
+++ apiZetta/src/main/java/com/zettaApp/apiZetta/resources/CargoResource.java
@@ -1,9 +1,17 @@
 package com.zettaApp.apiZetta.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +31,25 @@ public class CargoResource {
 		return cargoRepository.findAll();
 	}
 	
+	@GetMapping("/buscarPorId/{id}")
+	public Optional<Cargo> BuscarPorId(@PathVariable("id") long id){
+	 		return this.cargoRepository.findById(id);
+	}
 	
+	@PostMapping("/incluir")
+	public Cargo incluir(@RequestBody Cargo entity) {
+		return this.cargoRepository.save(entity);
+	}
 	
+	@PutMapping("/alterar")
+	public Cargo alterar(@RequestBody Cargo entity) {
+		return this.cargoRepository.save(entity);
+	}
+	
+	@DeleteMapping("/delete")
+	public void delete(@RequestBody Cargo entity) {
+		 this.cargoRepository.delete(entity);
+	}
+	
+
 }
